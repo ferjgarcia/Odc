ee45733212f3b09087057d3cb8970954a2c598d5
ferjgarcia <fernandojaviergarcia04@gmail.com>
2024-05-23 12:39:00 -0300

    Update auto.s

--- ejercicio1/auto.s
+++ ejercicio1/auto.s
@@ -50,15 +50,15 @@ rect_color: .word 0x808080   // Color gris en formato RGB
 _auto:
     // Configura las coordenadas del rectángulo
     mov x0, rect_color      // Color gris
-    mov x1, #100            // Coordenada X del extremo inferior izquierdo
-    mov x2, #100            // Coordenada Y del extremo inferior izquierdo
-    mov x3, #300            // Coordenada X del extremo superior derecho
-    mov x4, #300            // Coordenada Y del extremo superior derecho
+    mov x1, 100            // Coordenada X del extremo inferior izquierdo
+    mov x2, 100            // Coordenada Y del extremo inferior izquierdo
+    mov x3, 300            // Coordenada X del extremo superior derecho
+    mov x4, 300            // Coordenada Y del extremo superior derecho
 
     // Llama a la función Pinta_rectangulo
     bl Pinta_rectangulo
 
     // Salir del programa
-    mov x8, #93             // Syscall exit (número 93)
-    svc #0
+    mov x8, 93             // Syscall exit (número 93)
+    svc 0
 
