723c951c0d8b0dce681893c9c1a6e0ecd8ba0248
JuannSF <jsferreyra5@gmail.com>
2024-05-23 16:07:00 -0300

    Update auto.s

--- ejercicio1/auto.s
+++ ejercicio1/auto.s
@@ -3,24 +3,26 @@
 .globl _auto
 _auto:
 
-    // Configura las coordenadas del rectángulo
+    // Configura las coordenadas del auto
     ldr x0, =Rojo          // Cargar la dirección de Rojo (valor incluido en colores.s)
-    mov x1, #100            // Coordenada X del extremo inferior izquierdo
-    mov x2, #200            // Coordenada Y del extremo inferior izquierdo
-    mov x3, #155            // Coordenada X del extremo superior derecho
-    mov x4, #280            // Coordenada Y del extremo superior derecho
+    mov x1, 420            // Coordenada X del extremo inferior izquierdo
+    mov x2, 200            // Coordenada Y del extremo inferior izquierdo
+    mov x3, 155            // Coordenada X del extremo superior derecho
+    mov x4, 280            // Coordenada Y del extremo superior derecho
 
     // Llama a la función Pinta_rectangulo
     bl Pinta_rectangulo
 
     //RUEDAS
     ldr x0, =Linea_blanca_ruta          // Cargar la dirección de Rojo (valor incluido en colores.s)
-    mov x1, #100            // Coordenada X del extremo inferior izquierdo
-    mov x2, #20            // Coordenada Y del extremo inferior izquierdo
-    mov x3, #100            // Coordenada X del extremo superior derecho
-    mov x4, #28           // Coordenada Y del extremo superior derecho
+    mov x1, 100             // Coordenada X del extremo inferior izquierdo
+    mov x2, 200            // Coordenada Y del extremo inferior izquierdo
+    mov x3, 100            // Coordenada X del extremo superior derecho
+    mov x4, 280          // Coordenada Y del extremo superior derecho
 
     // Llama a la función Pinta_rectangulo
     bl Pinta_rectangulo
 
     ret
+
+ //LINEA VACIA
