7f054a4c1d079e27740613fb5e1deb3b3960d92e
DanielCabreraL <99976348+DanielCabreraL@users.noreply.github.com>
2024-05-23 11:20:36 -0300

    Update app.s

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -1,3 +1,12 @@
 // COLORES
 
-.equ Verde_ruta, 0x28CF19
+.equ Verde_pasto_1, 0x488800
+.equ Verde_pasto_2, 0x366600
+.equ Linea_roja_costado_1, 0x8E1F0E
+.equ Linea_roja_costado_2, 0x6D0C13
+.equ Linea_blanca_costado_1, 0xFEFEFC
+.equ Linea_blanca_costado_2, 0xB0B0B0
+.equ Linea_blanca_ruta, 0x6D0C13
+.equ Gris_ruta_1, 0x6D0C13
+.equ Verde_arbol_claro, 0x6D0C13
+.equ Verde_arbol_oscuro, 
