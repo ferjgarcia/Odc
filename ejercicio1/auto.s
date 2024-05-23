5ce034036a48c7f5335ed2ab847f729092cc9c14
ferjgarcia <fernandojaviergarcia04@gmail.com>
2024-05-23 16:28:24 -0300

    Update auto.s

--- ejercicio1/auto.s
+++ ejercicio1/auto.s
@@ -5,9 +5,9 @@ _auto:
 
     // Configura las coordenadas del auto
     ldr x0, =Rojo          // Cargar la dirección de Rojo (valor incluido en colores.s)
-    mov x1, 420            // Coordenada X del extremo inferior izquierdo
+    mov x1, 293            // Coordenada X del extremo inferior izquierdo
     mov x2, 200            // Coordenada Y del extremo inferior izquierdo
-    mov x3, 155            // Coordenada X del extremo superior derecho
+    mov x3, 347            // Coordenada X del extremo superior derecho
     mov x4, 280            // Coordenada Y del extremo superior derecho
 
     // Llama a la función Pinta_rectangulo
