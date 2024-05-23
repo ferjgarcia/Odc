91c7978f2d3d6510a581dd89ad4e3211ab2727a9
DanielCabreraL <99976348+DanielCabreraL@users.noreply.github.com>
2024-05-23 12:17:48 -0300

    Update vegetacion.s

--- ejercicio1/vegetacion.s
+++ ejercicio1/vegetacion.s
@@ -5,5 +5,27 @@
 .globl arboles
 
 Arboles:
+
+    str x0, [sp, -8]!
+    str x1, [sp, -8]!
+    str x2, [sp, -8]!
+    str x3, [sp, -8]!
+    str x4, [sp, -8]!
+    str x5, [sp, -8]!
+    str x6, [sp, -8]!
+    str x7, [sp, -8]!
+    str x8, [sp, -8]!
+    str x9, [sp, -8]!
     
 Arbustos:
+
+    str x0, [sp, -8]!
+    str x1, [sp, -8]!
+    str x2, [sp, -8]!
+    str x3, [sp, -8]!
+    str x4, [sp, -8]!
+    str x5, [sp, -8]!
+    str x6, [sp, -8]!
+    str x7, [sp, -8]!
+    str x8, [sp, -8]!
+    str x9, [sp, -8]!
