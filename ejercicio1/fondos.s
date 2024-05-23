c304285917e840dd7cc50add28b197474d5eea1c
DanielCabreraL <99976348+DanielCabreraL@users.noreply.github.com>
2024-05-23 16:32:35 -0300

    Update fondos.s

--- ejercicio1/fondos.s
+++ ejercicio1/fondos.s
@@ -1,15 +1,16 @@
 .section .text
 .include "colores.s"
 
-.globl _fondo
-_fondo:
+.globl pasto1
+pasto1:
 
     // Configura las coordenadas del rectángulo
-    ldr x0, = Verde_pasto_1         // Cargar la dirección de Verde_pasto_1 (valor incluido en colores.s)
-    mov x1, #0            // Coordenada X del extremo inferior izquierdo
-    mov x2, #479            // Coordenada Y del extremo inferior izquierdo
-    mov x3, #148            // Coordenada X del extremo superior derecho
-    mov x4, #0            // Coordenada Y del extremo superior derecho
+    ldr x0, = Verde_pasto_1          // Cargar la dirección de Rojo (si es un valor definido en .data)
+    
+    mov x1, 479            // Coordenada X del extremo inferior izquierdo
+    mov x2, 0            // Coordenada Y del extremo inferior izquierdo
+    mov x3, 148            // Coordenada X del extremo superior derecho
+    mov x4, 0            // Coordenada Y del extremo superior derecho
 
     // Llama a la función Pinta_rectangulo
     bl Pinta_rectangulo
