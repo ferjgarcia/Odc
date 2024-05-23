cb5ee2fd4bd01979e1723f1941dad472f42b22ad
ferjgarcia <fernandojaviergarcia04@gmail.com>
2024-05-23 17:43:38 -0300

    Update app.s

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -26,3 +26,39 @@ main:
  
     	b InfLoop
 //Fila vacia
+
+
+COMO LO HICE
+
+.include "colores.s"
+
+
+// Defines
+	.equ SCREEN_WIDTH, 640
+	.equ SCREEN_HEIGHT, 480
+	.equ BITS_PER_PIXEL, 32
+
+// Inicializo los registros
+
+	IniRegistros:
+		mov x29,x0
+		mov x19,SCREEN_WIDTH
+		mov x20,SCREEN_HEIGHT
+		ret
+
+// Loop infinito para poder ver el trabajo
+
+	InfLoop:
+		b InfLoop
+
+
+.globl main
+main:
+   	bl IniRegistros
+	
+	bl _fondo
+	bl _auto //ACA EJECUTAR LAS FUNCIONES
+ 
+    b InfLoop
+//Fila vacia
+
