9a653baa780a74134e975438108b42628fdcb676
ferjgarcia <fernandojaviergarcia04@gmail.com>
2024-05-23 15:48:50 -0300

    Update auto.s

--- ejercicio1/auto.s
+++ ejercicio1/auto.s
@@ -6,9 +6,9 @@ _auto:
     // Configura las coordenadas del rectángulo
     ldr x0, =Rojo          // Cargar la dirección de Rojo (valor incluido en colores.s)
     mov x1, #100            // Coordenada X del extremo inferior izquierdo
-    mov x2, #100            // Coordenada Y del extremo inferior izquierdo
-    mov x3, #300            // Coordenada X del extremo superior derecho
-    mov x4, #300            // Coordenada Y del extremo superior derecho
+    mov x2, #200            // Coordenada Y del extremo inferior izquierdo
+    mov x3, #155            // Coordenada X del extremo superior derecho
+    mov x4, #280            // Coordenada Y del extremo superior derecho
 
     // Llama a la función Pinta_rectangulo
     bl Pinta_rectangulo
