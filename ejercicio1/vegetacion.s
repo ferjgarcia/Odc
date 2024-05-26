8f178a666811e22fbc84b1b3b420730ce0b3d597
JuannSF <jsferreyra5@gmail.com>
2024-05-26 19:06:06 -0300

    Update vegetacion.s

--- ejercicio1/vegetacion.s
+++ ejercicio1/vegetacion.s
@@ -1,31 +1,46 @@
 .section .text
+.include "colores.s"
 
-// ÁRBOLES
+.globl _arboles
+_arboles:
 
-.globl arboles
+    str x0,[sp,-8]!
+    str x1,[sp,-8]!
+    str x2,[sp,-8]!
+    str x3,[sp,-8]!
+    str x23,[sp,-8]!
+    str x24,[sp,-8]!
+    str x25,[sp,-8]!
+    str x30,[sp,-8]!
+    // COPA ARBOL
+        ldr x0,=Verde_arbol_claro
+		mov x1, 600  // X
+		mov x2, 100  // Y
+		mov x3, 20 // Radio
+	bl Pinta_circulo
+    // TRONCO
+        ldr x0, =Tronco_claro          // Cargar la dirección de Tronco_claro (valor incluido en colores.s)
+        mov x1, 598            // Coordenada X del extremo inferior izquierdo
+        mov x2, 120            // Coordenada Y del extremo inferior izquierdo
+        mov x3, 603           // Coordenada X del extremo superior derecho
+        mov x4, 130            // Coordenada Y del extremo superior derecho
 
-Arboles:
+    bl Pinta_rectangulo
 
-    str x0, [sp, -8]!
-    str x1, [sp, -8]!
-    str x2, [sp, -8]!
-    str x3, [sp, -8]!
-    str x4, [sp, -8]!
-    str x5, [sp, -8]!
-    str x6, [sp, -8]!
-    str x7, [sp, -8]!
-    str x8, [sp, -8]!
-    str x9, [sp, -8]!
-    
-Arbustos:
+    // ARBUSTOS
+        ldr x0,=Verde_arbol_claro
+		mov x1, 570  // X
+		mov x2, 200  // Y
+		mov x3, 10
+	bl Pinta_circulo
 
-    str x0, [sp, -8]!
-    str x1, [sp, -8]!
-    str x2, [sp, -8]!
-    str x3, [sp, -8]!
-    str x4, [sp, -8]!
-    str x5, [sp, -8]!
-    str x6, [sp, -8]!
-    str x7, [sp, -8]!
-    str x8, [sp, -8]!
-    str x9, [sp, -8]!
+    ldr x30,[sp],8
+    ldr x25,[sp],8
+    ldr x24,[sp],8
+    ldr x23,[sp],8
+    ldr x3,[sp],8
+    ldr x2,[sp],8
+    ldr x1,[sp],8
+    ldr x0,[sp],8
+
+    ret
