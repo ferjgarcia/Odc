25429ba49d78edc5b0b7297e49a718b681fb6859
JuannSF <jsferreyra5@gmail.com>
2024-06-05 10:32:30 -0300

    Update auto.s

--- ejercicio2/auto.s
+++ ejercicio2/auto.s
@@ -1,3 +1,4 @@
+    
 .section .text
 .include "colores.s"
 .globl _auto
@@ -25,10 +26,10 @@ _auto:
 
     // Inicializa las coordenadas de la primera rueda
     ldr x0, =Ruedas        // Cargar la dirección de Ruedas (valor incluido en colores.s)
-    mov x5, 287            // Coordenada X inicial del extremo inferior izquierdo
-    mov x6, 215            // Coordenada Y inicial del extremo inferior izquierdo
-    mov x7, 353            // Coordenada X inicial del extremo superior derecho
-    mov x8, 235            // Coordenada Y inicial del extremo superior derecho
+    mov x5,640+ 287            // Coordenada X inicial del extremo inferior izquierdo
+    mov x6,480+  215            // Coordenada Y inicial del extremo inferior izquierdo
+    mov x7,640+ 353            // Coordenada X inicial del extremo superior derecho
+    mov x8,480+ 235            // Coordenada Y inicial del extremo superior derecho
 
     sub x5, x5, x10 // X
     sub x7, x7, x10 // X
@@ -56,15 +57,15 @@ ruedas_loop:
     cmp w9, #2
     b.lt ruedas_loop       // Si aún no se han dibujado 4 ruedas, repite el bucle
 
-    //LUCES DEL AUTO
+ /*   //LUCES DEL AUTO
     // Inicializar coordenadas del primer triángulo
     ldr x0, =Amarillo
-    mov x1, 303                   // Coordenada X del vértice superior
-    mov x2, 203                  // Coordenada Y del vértice superior
-    mov x3, 278                  // Coordenada X del primer vértice de la base
-    mov x4, 153                  // Coordenada Y del primer vértice de la base
-    mov x5, 328                  // Coordenada X del segundo vértice de la base
-    mov x6, 153                  // Coordenada Y del segundo vértice de la base
+    mov x1, 640+ 303                  // Coordenada X del vértice superior
+    mov x2, 480+ 203                  // Coordenada Y del vértice superior
+    mov x3, 640+ 278                  // Coordenada X del primer vértice de la base
+    mov x4, 480+ 153                  // Coordenada Y del primer vértice de la base
+    mov x5, 640+ 328                  // Coordenada X del segundo vértice de la base
+    mov x6, 480+ 153                  // Coordenada Y del segundo vértice de la base
     sub x2, x2, x11 // Y
     sub x4, x4, x11 // y
     sub x6, x6, x11 // Y
@@ -73,25 +74,25 @@ ruedas_loop:
 
     // Inicializar coordenadas del segundo triángulo
     
-    mov x1, 338                  // Coordenada X del vértice superior del triángulo
-    mov x2, 203                  // Coordenada Y del vértice superior del triángulo
-    mov x3, 313                  // Coordenada X del primer vértice de la base del triángulo
-    mov x4, 153                  // Coordenada Y del primer vértice de la base del triángulo
-    mov x5, 363                  // Coordenada X del segundo vértice de la base del triángulo
-    mov x6, 153                  // Coordenada Y del segundo vértice de la base del triángulo
+    mov x1, 640+ 338                  // Coordenada X del vértice superior del triángulo
+    mov x2, 480+ 203                  // Coordenada Y del vértice superior del triángulo
+    mov x3, 640+ 313                  // Coordenada X del primer vértice de la base del triángulo
+    mov x4, 480+ 153                  // Coordenada Y del primer vértice de la base del triángulo
+    mov x5, 640+ 363                  // Coordenada X del segundo vértice de la base del triángulo
+    mov x6, 480+ 153                  // Coordenada Y del segundo vértice de la base del triángulo
     sub x2, x2, x11 // Y
     sub x4, x4, x11 // y
     sub x6, x6, x11 // Y
     bl Pinta_triangulo
-
+*/
     //CUERPO DEL AUTO
 
     // Configura las coordenadas del auto
     mov x0, x12          // Cargar la dirección de Rojo (valor incluido en colores.s)
-    mov x1, 293            // Coordenada X del extremo inferior izquierdo
-    mov x2, 203            // Coordenada Y del extremo inferior izquierdo
-    mov x3, 348            // Coordenada X del extremo superior derecho
-    mov x4, 290            // Coordenada Y del extremo superior derecho
+    mov x1, 640+ 293            // Coordenada X del extremo inferior izquierdo
+    mov x2, 480+ 203            // Coordenada Y del extremo inferior izquierdo
+    mov x3, 640+ 348            // Coordenada X del extremo superior derecho
+    mov x4, 480+ 290            // Coordenada Y del extremo superior derecho
 
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
@@ -103,10 +104,10 @@ ruedas_loop:
     bl Pinta_rectangulo
     
     mov x0, x12
-    mov x1, 296            
-    mov x2, 200            
-    mov x3, 345            
-    mov x4, 203            
+    mov x1, 640+ 296            
+    mov x2, 480+ 200            
+    mov x3, 640+ 345            
+    mov x4, 480+ 203            
 
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
@@ -117,10 +118,10 @@ ruedas_loop:
     bl Pinta_rectangulo
 
     mov x0, x12
-    mov x1, 302            
-    mov x2, 197            
-    mov x3, 339            
-    mov x4, 200           
+    mov x1, 640+ 302            
+    mov x2, 480+ 197            
+    mov x3, 640+ 339            
+    mov x4, 480+ 200           
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -132,10 +133,10 @@ ruedas_loop:
     // Luces
 
     ldr x0, =Luces
-    mov x1, 302            
-    mov x2, 197            
-    mov x3, 312            
-    mov x4, 200           
+    mov x1, 640+ 302            
+    mov x2, 480+ 197            
+    mov x3, 640+ 312            
+    mov x4, 480+ 200           
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -145,10 +146,10 @@ ruedas_loop:
     bl Pinta_rectangulo
 
     ldr x0, =Luces
-    mov x1, 298            
-    mov x2, 200            
-    mov x3, 302            
-    mov x4, 203           
+    mov x1, 640+ 298            
+    mov x2, 480+ 200            
+    mov x3, 640+ 302            
+    mov x4, 480+ 203           
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -158,10 +159,10 @@ ruedas_loop:
     bl Pinta_rectangulo
 
     ldr x0, =Luces
-    mov x1, 329            
-    mov x2, 197            
-    mov x3, 339            
-    mov x4, 200           
+    mov x1, 640+ 329            
+    mov x2, 480+ 197            
+    mov x3, 640+ 339            
+    mov x4, 480+ 200           
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -171,10 +172,10 @@ ruedas_loop:
     bl Pinta_rectangulo
 
     ldr x0, =Luces
-    mov x1, 339            
-    mov x2, 200            
-    mov x3, 343            
-    mov x4, 203           
+    mov x1, 640+ 339            
+    mov x2, 480+ 200            
+    mov x3, 640+ 343            
+    mov x4, 480+ 203           
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -186,10 +187,10 @@ ruedas_loop:
     // Vidrios delanteros
 
     ldr x0, =Vidrios       // Cargar la dirección de Vidrios (valor incluido en colores.s)
-    mov x1, 302
-    mov x2, 218            
-    mov x3, 339            
-    mov x4, 230            
+    mov x1, 640+ 302
+    mov x2, 480+ 218            
+    mov x3, 640+ 339            
+    mov x4, 480+ 230            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -198,10 +199,10 @@ ruedas_loop:
     bl Pinta_rectangulo
 
     ldr x0, =Vidrios       
-    mov x1, 305            
-    mov x2, 215            
-    mov x3, 336            
-    mov x4, 218            
+    mov x1, 640+ 305            
+    mov x2, 480+ 215            
+    mov x3, 640+ 336            
+    mov x4, 480+ 218            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -210,10 +211,10 @@ ruedas_loop:
     bl Pinta_rectangulo    
 
     mov x0, x12       
-    mov x1, 305            
-    mov x2, 229            
-    mov x3, 336            
-    mov x4, 230            
+    mov x1, 640+ 305            
+    mov x2, 480+ 229            
+    mov x3, 640+ 336            
+    mov x4, 480+ 230            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -224,10 +225,10 @@ ruedas_loop:
     // Vidrios traseros
 
     ldr x0, =Vidrios       // Cargar la dirección de Vidrios (valor incluido en colores.s)
-    mov x1, 302            
-    mov x2, 271            
-    mov x3, 339            
-    mov x4, 283            
+    mov x1, 640+ 302            
+    mov x2, 480+ 271            
+    mov x3, 640+ 339            
+    mov x4, 480+ 283            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -236,10 +237,10 @@ ruedas_loop:
     bl Pinta_rectangulo
 
     ldr x0, =Vidrios       
-    mov x1, 305            
-    mov x2, 283            
-    mov x3, 336            
-    mov x4, 284            
+    mov x1, 640+ 305            
+    mov x2, 480+ 283            
+    mov x3, 640+ 336            
+    mov x4, 480+ 284            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -248,10 +249,10 @@ ruedas_loop:
     bl Pinta_rectangulo    
 
     mov x0, x12       
-    mov x1, 305            
-    mov x2, 270            
-    mov x3, 336            
-    mov x4, 271            
+    mov x1, 640+ 305            
+    mov x2, 480+ 270            
+    mov x3, 640+ 336            
+    mov x4, 480+ 271            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -262,10 +263,10 @@ ruedas_loop:
     // Vidrios laterales  
 
     ldr x0, =Vidrios       
-    mov x1, 302            
-    mov x2, 267            
-    mov x3, 304            
-    mov x4, 234            
+    mov x1, 640+ 302            
+    mov x2, 480+ 267            
+    mov x3, 640+ 304            
+    mov x4, 480+ 234            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -274,10 +275,10 @@ ruedas_loop:
     bl Pinta_rectangulo
 
     mov x0, x12       
-    mov x1, 302            
-    mov x2, 251            
-    mov x3, 304            
-    mov x4, 250            
+    mov x1, 640+ 302            
+    mov x2, 480+ 251            
+    mov x3, 640+ 304            
+    mov x4, 480+ 250            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -286,10 +287,10 @@ ruedas_loop:
     bl Pinta_rectangulo
 
     ldr x0, =Vidrios       
-    mov x1, 339            
-    mov x2, 267            
-    mov x3, 337            
-    mov x4, 234            
+    mov x1, 640+ 339            
+    mov x2, 480+ 267            
+    mov x3, 640+ 337            
+    mov x4, 480+ 234            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -298,10 +299,10 @@ ruedas_loop:
     bl Pinta_rectangulo
 
     mov x0, x12       
-    mov x1, 339            
-    mov x2, 251            
-    mov x3, 337            
-    mov x4, 250            
+    mov x1, 640+ 339            
+    mov x2, 480+ 251            
+    mov x3, 640+ 337            
+    mov x4, 480+ 250            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -312,10 +313,10 @@ ruedas_loop:
     // ALERÓN
  
     ldr x0, =Ruedas       // Cargar la dirección de Rojo_Sombra (valor incluido en colores.s)
-    mov x1, 290            
-    mov x2, 290            
-    mov x3, 351            
-    mov x4, 297
+    mov x1, 640+ 290            
+    mov x2, 480+ 290            
+    mov x3, 640+ 351            
+    mov x4, 480+ 297
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -324,10 +325,10 @@ ruedas_loop:
     bl Pinta_rectangulo
 
     mov x0, x12       
-    mov x1, 290            
-    mov x2, 293            
-    mov x3, 351            
-    mov x4, 299            
+    mov x1, 640+ 290            
+    mov x2, 480+ 293            
+    mov x3, 640+ 351            
+    mov x4, 480+ 299            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
@@ -338,10 +339,10 @@ ruedas_loop:
     // Líneas verticales del auto
    
     ldr x0, =Amarillo_sombra       // Cargar la dirección de Amarillo_sombra (valor incluido en colores.s)
-    mov x1, 316            
-    mov x2, 271            
-    mov x3, 325            
-    mov x4, 229            
+    mov x1, 640+ 316            
+    mov x2, 480+ 271            
+    mov x3, 640+ 325            
+    mov x4, 480+ 229            
     // DESPLAZA EL AUTO A UNA POSICION (X, Y)
     sub x1, x1, x10 // X
     sub x3, x3, x10 // X
